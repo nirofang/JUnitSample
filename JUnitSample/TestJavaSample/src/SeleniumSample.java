0f4fbe81f134eab67adc22f9e44745127536aafe
Niro Fang <nirofang@outlook.com>
2018-02-10 09:56:22 +0800

    Add selenium webdriver test

---
+++ JUnitSample/TestJavaSample/src/SeleniumSample.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import youtube.edurka.selenium.webdriver.basic.Day1;
+
+public class SeleniumSample {
+
+	@Test
+	void test() {
+		Day1 myObj = new Day1();
+		myObj.invokeBrowser();
+		//fail("Not yet implemented");
+	}
+
+}
