f2fdb1807b136b3f9f5a9cd522d31635454a2af7
Niro Fang <nirofang@outlook.com>
2018-02-14 13:43:02 +0800

    Arrange resource2

---
+++ JavaSampleTest/src/SeleniumEdurkaSample.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import youtube.edurka.selenium.webdriver.basic.Day1;
+
+public class SeleniumEdurkaSample {
+
+	@Test
+	void test() {
+		Day1 myObj = new Day1();
+		myObj.invokeBrowser();
+		//fail("Not yet implemented");
+	}
+
+}
